3e539c80b5be3af33ef230b8eb60a397b53a1bf1
Wellington Rodrigues <wellingtonrodrigues.rj@gmail.com>
2022-12-14 16:12:46 -0300

    dominio dos dados

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
 
 export default function App() {
+
+  const [local, setLocal] = useState([
+    {
+      Id: 1,
+      Nome: "",
+      Localizacao: "",
+      cidade: "",
+      bairro: "",
+      cep: "",
+      descricao: "",
+      fotocapa: "",
+      foto: "",
+      preçodiario: 0,
+      classificacao: 5,
+      telefone: "",
+      email: ""
+    }
+  ]);
+
   return (
     <div>Ola</div>
   );
